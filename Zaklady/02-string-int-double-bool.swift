a58e1e6fc577958eec549ae4c9bd22bacc8a89a8
pavelsurovy <surolm@centrum.sk>
2022-04-21 21:17:11 +0200

    pridane string, int, double a float

---
+++ Zaklady/02-string-int-double-bool.swift
@@ -0,0 +1,52 @@
+
+// String
+
+let silaVetra = "mierny vietor"
+let automat = "vyhral som na 🎰"
+
+let citacia = "On povedal \"STOJ\" a tak zostal stáť...!"
+
+let recept = """
+500g múky
+250g cukru
+1 vajco
+0.2 litra mlieka
+"""
+
+print(silaVetra.count)
+
+let silaVetraVelkym = silaVetra.uppercased()
+let citaciaMalym = citacia.lowercased()
+
+
+// Integer - Int
+
+let vstupne = 5
+
+let zostatokNaUcte = 1_000_000_000_000_000
+
+//UInt
+
+// Double
+
+let pi = 3.141592654
+let vydavok = 5.50
+let teplota = -15.5
+
+//Float
+
+// Bool
+
+let rybaPlavaVoVode = true
+let kolesoJeStvorec = false
+
+automat.isEmpty
+
+let obrazok = "fotka.jpg"
+obrazok.hasPrefix("fotka")
+obrazok.hasSuffix(".jpg")
+
+let sto = 101
+sto.isMultiple(of: 2)
+
+100.isMultiple(of: 4)
