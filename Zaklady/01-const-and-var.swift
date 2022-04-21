929f7999a96be7132d003e159c16bc4582129e71
pavelsurovy <surolm@centrum.sk>
2022-04-21 20:59:49 +0200

    pridane konstanty a premenne

---
+++ Zaklady/01-const-and-var.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// var cislo = "4"
+var cislo = 4; cislo + 2
+
+var meno = "Aikim"
+meno = "Delli"
+meno = "Tara"
+
+let postava = "Harvey Specter"
+// postava = "Donna Paulsen"
+
+var pracovnaPozicia = "Predavač"
+print(pracovnaPozicia)
+
+pracovnaPozicia = "Manažér"
+print(pracovnaPozicia)
+
+let oblubenyNapoj = "Voda"
+let rasaPsa = "vyžla"
+let akoDlhoBudeEsteTrvatTotoVideo = "nebojte, už končíme"
+
+var moja_premenna1 = "iba moja"
+//var 123 = "ABC"
+var `repeat` = "opakuj"
+
+let 🧑🏼‍💻 = "pracujem ako 🧑🏼‍💻"
+
+// print("nic nevypise, lebo kod je zakomentovany")
+
+// toto je aka pekna premenna
+var obrazok = "kozicky.jpg" // komentar funguje aj tu...
+
+/*
+ kod ktory nezbehne
+ alebo komentar
+ pre citatela
+ */
+
+//let oblubenyNapoj = "Voda"
+//let rasaPsa = "vyžla"
+//let akoDlhoBudeEsteTrvatTotoVideo = "nebojte, už končíme"
+
+kod ktory nezbehne
+alebo komentar
+pre citatela
