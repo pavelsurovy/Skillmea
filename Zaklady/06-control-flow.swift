f553398cac72e10f9d4830ab9bb7c5ae7f34a64f
pavelsurovy <surolm@centrum.sk>
2022-05-02 17:34:37 +0200

    pridany for-in cyklus

---
+++ Zaklady/06-control-flow.swift
@@ -0,0 +1,38 @@
+// For-in Loop
+
+let pracovnaPozicia = ["automechanik", "kominár", "IT technik", "stolár"]
+
+for praca in pracovnaPozicia {
+    print("pracujem ako \(praca)")
+}
+
+0...5
+0..<6
+
+for i in 0...5 {
+    print("\(i) + 1 = \(i + 1)")
+}
+
+for _ in 1..<6 {
+    print("Mám ťa rád!")
+}
+
+for i in 1...10 {
+    if i == 5 { continue }
+    print(i)
+}
+print("pokračujeme v kóde po cykle...")
+
+let subory = ["aikim.jpg", "delli.jpg", "heslo.txt", "kozicky.png"]
+
+for subor in subory {
+    if !subor.hasSuffix(".jpg") && !subor.hasSuffix(".png") { continue }
+    print("Našiel som fotku: \(subor)")
+}
+
+for i in 1...5 {
+    for j in 1...5 {
+        print("\(i) + \(j) = \(i + j)")
+    }
+    print()
+}
