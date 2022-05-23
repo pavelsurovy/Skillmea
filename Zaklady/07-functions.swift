54896dc06e662b49bc206eb16a9c233d872f4550
Pavel Surovy <pavel.surovy@gmail.com>
2022-05-23 13:43:47 +0200

    pridane navratove funkcie

--- Zaklady/07-functions.swift
+++ Zaklady/07-functions.swift
@@ -41,3 +41,45 @@ pridaj()
 pridaj()
 
 print(skore)
+
+
+// Returning Functions
+
+import Foundation
+
+let druhaOdmocnina = sqrt(441)
+print(druhaOdmocnina)
+
+func priemernaTeplota(rannaTeplota: Double, obednaTeplota: Double, vecernaTeplota: Double) -> Double {
+    let priemer = (rannaTeplota + obednaTeplota + vecernaTeplota) / 3
+    return priemer
+}
+
+let dennaTeplota = priemernaTeplota(rannaTeplota: 1.7, obednaTeplota: 7.3, vecernaTeplota: -3.6)
+print("Dňa 3. 1. 2022 bola priemerná teplota: \(dennaTeplota")
+
+func priemerTestov(ziak: String, hodnotenie: [Int]) -> String {
+    var vysledok = "\(ziak) má z testov priemer: "
+    var priemer = 0
+
+    for znamka in hodnotenie {
+        priemer += znamka
+    }
+
+    priemer /= hodnotenie.count
+
+    vysledok += String(priemer)
+    vysledok += " bodov"
+
+    return vysledok
+}
+
+let juraj = priemerTestov(ziak: "Juraj", hodnotenie: [80, 92, 75, 70, 88])
+print(juraj)
+
+func spocitaj(retazec: String) -> Int {
+    return retazec.count
+}
+
+
+print(spocitaj(retazec: juraj))
