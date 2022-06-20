9d0a1905db942310dbde0e1bcfcbdc107d5679f7
pavelsurovy <surolm@centrum.sk>
2022-06-20 14:31:24 +0200

    pridany static

--- Zaklady/12-access-control.swift
+++ Zaklady/12-access-control.swift
@@ -22,3 +22,29 @@ print(zasoba.psieGranule)
 
 //zasoba.psieGranule = 5_000_000_000
 print(zasoba.psieGranule)
+
+
+// Static
+
+import SwiftUI
+
+struct Beh {
+    static var dnesneKM = 10
+
+    static func poBehu() -> String {
+        return "Dnes sa bežalo skvele"
+    }
+}
+
+print(Beh.dnesneKM)
+print(Beh.poBehu())
+
+struct Farby {
+    static let cervena = Color(red: 1, green: 0, blue: 0)
+    static let zelena = Color(red: 0, green: 1, blue: 0)
+    static let modra = Color(red: 0, green: 0, blue: 1)
+}
+
+Farby.cervena
+Farby.zelena
+Farby.modra
