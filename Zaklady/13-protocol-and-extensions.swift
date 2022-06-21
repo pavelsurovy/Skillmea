8d69b0eca5d49b33ca3af33e5a8359e0188c313f
pavelsurovy <surolm@centrum.sk>
2022-06-21 13:24:34 +0200

    pridany extensions

--- Zaklady/13-protocol-and-extensions.swift
+++ Zaklady/13-protocol-and-extensions.swift
@@ -33,3 +33,41 @@ class Lietadlo: LietadlaProtocol {
 class Airbus: Lietadlo {
 
 }
+
+
+// Extensions
+
+extension Int {
+    func objem() -> Int {
+        self * self * self
+    }
+}
+
+3.objem()
+
+func objem(pre cislo: Int) -> Int {
+    return cislo * cislo * cislo
+}
+
+objem(pre: 7)
+
+extension Auto {
+    init(model: String, spotreba: Double, nadrz: Double) {
+        self.model = model
+        self.spotreba = spotreba
+        self.nadrz = nadrz
+        self.dojazd = (nadrz / spotreba) * 100
+    }
+}
+
+struct Auto {
+    let model: String
+    let spotreba: Double
+    let nadrz: Double
+    let dojazd: Double
+}
+
+let seat = Auto(model: "leon", spotreba: 5.4, nadrz: 42)
+
+let polo = Auto(model: "Polo", spotreba: 4.8, nadrz: 46)
+let scout = Auto(model: "Octavia Scout", spotreba: 6.5, nadrz: 55, dojazd: 700)
