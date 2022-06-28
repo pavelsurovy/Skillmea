5bea72ea243de83e8a9102b7548986d330d1e789
pavelsurovy <surolm@centrum.sk>
2022-06-28 13:00:14 +0200

    pridany nil a optionals

---
+++ Zaklady/14-nil-and-optionals.swift
@@ -0,0 +1,15 @@
+
+// Nil and Optionals
+
+var mojaPremnna: String?
+print(mojaPremnna)
+
+print(type(of: mojaPremnna))
+
+var cislo: Optional<Int>
+
+let iPhone: String? = nil
+
+var jeden: Int? = 1
+var dva: Int = 2
+print(jeden + dva)
