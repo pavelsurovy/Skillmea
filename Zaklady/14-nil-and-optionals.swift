75f09c855284c3dde3fedaf31159d44da14388d6
pavelsurovy <surolm@centrum.sk>
2022-06-28 13:33:49 +0200

    pridany if let

--- Zaklady/14-nil-and-optionals.swift
+++ Zaklady/14-nil-and-optionals.swift
@@ -13,3 +13,25 @@ let iPhone: String? = nil
 var jeden: Int? = 1
 var dva: Int = 2
 print(jeden + dva)
+
+
+var cislo: Int?
+cislo = 8
+
+// IF LET
+
+if let rozbaleneCislo = cislo {
+    print("Zadaná hodnota: \(rozbaleneCislo)")
+} else {
+    print("Hodnota je nil!")
+}
+
+if let cislo = cislo {
+    print("Máme hodnotu: \(cislo)")
+}
+
+print(cislo)
+
+if let cislo = cislo {
+    print("zase číslo: \(cislo)")
+}
