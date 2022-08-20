020139cae52a6eb0278fbe01c78a27b82ebc7123
pavelsurovy <surolm@centrum.sk>
2022-08-20 21:33:30 +0200

    vypracovana DU2 array set dictionary

---
+++ Domace ulohy/DU-02-array-set-dictionary.swift
@@ -0,0 +1,25 @@
+// Zoraď čísla od najmenšieho po najväčšie.
+let arr = [0, 34, -14, -43, -1, 38, -8, 26, 44, 43, 33, 50, -45, 2, -13, -38, -6, -50, -46, 49, -39, 21, 19, 10, 42, -40]
+
+arr.sorted()
+
+// 1) zisti ci sa "duic" nachadza v Array
+// 2) aký index má slovo "eros"
+
+let arrSlova = ["tempus", "nulla", "cras", "orci", "vel", "non", "commodo", "imperdiet", "enim", "eu", "varius", "amet", "tempus", "id", "quam", "vulputate", "nunc", "morbi", "lectus", "fringilla", "elit", "augue", "eros", "sagittis", "dolor", "donec", "ultrices", "mattis", "ultricies", "elit", "fringilla", "augue", "quis", "sagittis", "ipsum", "accumsan", "mauris", "tincidunt", "malesuada", "venenatis", "felis", "leo", "nec", "non", "hendrerit", "auctor", "efficitur", "laoreet", "id", "quisque", "vitae", "libero", "morbi", "felis", "eu", "condimentum", "ut", "magna", "quis", "arcu", "ex", "mi", "duis", "pulvinar", "vitae", "suscipit", "tempus", "non", "bibendum", "nullam", "volutpat", "aliquam", "vivamus", "sed", "quam", "velit", "ultricies", "ut", "auctor", "magna", "ligula", "nec", "nisi", "rutrum", "sem", "sed", "a", "interdum", "fringilla", "viverra", "ullamcorper", "nibh", "maecenas", "pulvinar", "vestibulum", "odio", "sit", "vel", "metus", "fusce"]
+
+let arrPrvok1 = [43, 21, 92, 75, 68, 86, 9, 52, 57, 5, 5, 69, 29, 63, 12, 27, 97, 7, 88, 93, 96, 18, 57, 58, 64, 92, 62, 27, 90, 16, 10, 77, 1, 56, 60, 61, 62, 7, 39, 23, 81, 47, 27, 87, 19, 20, 28, 9, 99, 61]
+let arrPrvok2 = [89, 1, 30, 81, 19, 31, 50, 10, 61, 24, 72, 22, 7, 37, 46, 59, 34, 40, 51, 90, 60, 0, 54, 7, 86, 12, 13, 86, 74, 87, 94, 19, 26, 43, 22, 72, 45, 5, 7, 42, 74, 53, 89, 87, 97, 52, 45, 17, 36, 66]
+
+let nachadza = arrSlova.contains("duic")
+let pozicia = arrSlova.firstIndex(of: "eros")
+
+// nájdi unikátny prvok, ktorý sa nachádza v oboch poliach
+
+let prv1 = [321, 270, 229, 161, 378, 148, 316, 152, 180, 108, 188, 225, 333, 358, 189, 112, 196, 373, 252, 385, 239, 363, 171, 191, 155, 338, 117, 255, 388, 259, 272, 355, 266, 203, 220, 118, 124, 209, 170, 219, 136, 274, 357, 317, 256, 320, 356, 397, 315, 176, 305, 182, 347, 336, 185, 172, 398, 121, 318, 212, 127, 132, 122]
+let prv2 = [311, 159, 138, 461, 389, 226, 158, 370, 130, 465, 221, 376, 485, 169, 246, 224, 222, 450, 430, 113, 364, 245, 398, 282, 314, 116, 324, 446, 371, 156, 428, 175, 125, 346, 360, 162, 238, 395, 412, 354, 285, 232, 143, 140, 269, 303, 443, 133, 243, 210, 448, 381, 417, 301, 258, 268, 200, 195, 394, 483, 260, 141, 383]
+
+let prvy = Set(prv1)
+let druhy = Set(prv2)
+
+let zhodnyPrvok = druhy.intersection(prvy)
