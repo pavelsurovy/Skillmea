1b5eace081e1805032a90dc9b5d6ad469cc53b43
pavelsurovy <surolm@centrum.sk>
2022-09-04 22:00:50 +0200

    odobrate zbytocne zatvorky

--- Domace ulohy/DU-03-control-flow.swift
+++ Domace ulohy/DU-03-control-flow.swift
@@ -2,13 +2,13 @@
 
 // Vzorec na výpočet priestupného roku je naslevoný:
 // 1. rok musí byť deliteľný 4
-// 2. následne ale je deliteľný 400, ale nie je deliteľný 100.
+// 2. následne ale je deliteľný 400, alebo nie je deliteľný 100.
 // priestupné roky boli napríklad: 1984, 1992, 2000
 // nepriestupné roky sú napríklad 1500, 1700 a 2100
 
 let rok = 1954
 
-if ((rok % 4 == 0) && ((rok % 400 == 0) || (rok % 100 != 0))) {
+if rok % 4 == 0 && (rok % 400 == 0 || rok % 100 != 0) {
     print("Rok \(rok) JE priestupný!")
 } else {
     print("Rok \(rok) NIE JE priestupný!")
